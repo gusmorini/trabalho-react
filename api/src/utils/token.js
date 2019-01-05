1ddd835bc9a59bafcf9e0c1a6a011e133bbc946c
Gustavo Morini <gustavommh@gmail.com>
2019-01-05 15:22:01 -0200

    first

---
+++ api/src/utils/token.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = '&uaQ76gR#SQPthHV82#Dt=HnUwzbM8KnP&T#uTvG*NsQZMspRt';
+
+function autenticarToken (request, response, next){
+
+    //const { cookies: {token} } = request;
+    const token = request.headers["x-access-token"] || request.cookies["x-access-token"];
+
+    console.log(token);
+
+    try {
+        const payload = jwt.verify(token, SECRET_KEY);
+        console.log('Token válido', payload);
+        request.usuarioLogado = payload;
+        next();
+        //response.status(200).send('Acesso permitido');
+    }
+    catch (exception) {
+        console.error('Token inválido', exception);
+        response.status(403).send('Acesso negado, faça login e tente novamente...');
+    }
+
+}
+
+function gerarToken (usuario){
+	const { id, nome, email, cpf, nascimento } = usuario
+	const payload = { id, nome, email, cpf, nascimento}
+	return jwt.sign(payload, SECRET_KEY);
+}
+
+module.exports = {
+	gerarToken,
+	autenticarToken
+}
